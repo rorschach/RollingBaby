68de55348a9d9d121d9aa5df2c1c3f78a4f7e7e5
rorschach <1265343198@qq.com>
2015-08-25 08:47:38 +0800

    Display all sub-views in DialogFragment

--- app/src/main/java/com/hl/rollingbaby/ui/SettingActivity.java
+++ app/src/main/java/com/hl/rollingbaby/ui/SettingActivity.java
@@ -1,66 +1,169 @@
 package com.hl.rollingbaby.ui;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.hl.rollingbaby.R;
 import com.hl.rollingbaby.bean.Constants;
 
-public class SettingActivity extends ActionBarActivity {
+public class SettingActivity extends PreferenceActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     public static final String TAG = "SettingActivity";
     public static final String IP_ADDRESS_PORT = "IP_ADDRESS_PORT";
 
+    private Toolbar mActionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_setting);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        toolbar.setTitle(R.string.app_name);
-//        setSupportActionBar(toolbar);
-        getFragmentManager().beginTransaction().replace(
-                android.R.id.content, new MyPreferenceFragment()).commit();
+
+        addPreferencesFromResource(R.xml.preferences);
+        //other preference initialization
+
+        mActionBar.setTitle(getTitle());
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment implements
-            SharedPreferences.OnSharedPreferenceChangeListener {
-        @Override
-        public void onCreate(final Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-        }
-//
-        @Override
-        public void onResume() {
-            super.onResume();
-            getPreferenceScreen().getSharedPreferences()
-                    .registerOnSharedPreferenceChangeListener(this);
-            Log.d(TAG, "onResume");
-        }
+    @Override
+    public void setContentView(int layoutResID) {
+        ViewGroup contentView = (ViewGroup) LayoutInflater.from(this).inflate(
+                R.layout.activity_setting, new LinearLayout(this), false);
 
-        @Override
-        public void onPause() {
-            super.onPause();
-            getPreferenceScreen().getSharedPreferences()
-                    .unregisterOnSharedPreferenceChangeListener(this);
-            Log.d(TAG, "onPause");
-        }
-
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-                                              String key) {
-            if (key.equals(IP_ADDRESS_PORT)) {
-                Preference pref = this.findPreference(key);
-                // Set summary to be the user-description for the selected value
-                pref.setSummary(getActivity().getResources().getString(R.string.summary_header)
-                        + sharedPreferences.getString(key, Constants.ADDRESS_PORT));
-                Log.d("TAG", pref.getSummary().toString());
+        mActionBar = (Toolbar) contentView.findViewById(R.id.action_bar);
+        mActionBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
-            Log.d(TAG, "onSharedPreferenceChanged");
-        }
+        });
+
+        ViewGroup contentWrapper = (ViewGroup) contentView.findViewById(R.id.content_wrapper);
+        LayoutInflater.from(this).inflate(layoutResID, contentWrapper, true);
+
+        getWindow().setContentView(contentView);
     }
 
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        // Allow super to try and create a view first
+        final View result = super.onCreateView(name, context, attrs);
+        if (result != null) {
+            return result;
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            // If we're running pre-L, we need to 'inject' our tint aware Views in place of the
+            // standard framework versions
+            switch (name) {
+                case "EditText":
+                    return new AppCompatEditText(this, attrs);
+                case "Spinner":
+                    return new AppCompatSpinner(this, attrs);
+                case "CheckBox":
+                    return new AppCompatCheckBox(this, attrs);
+                case "RadioButton":
+                    return new AppCompatRadioButton(this, attrs);
+                case "CheckedTextView":
+                    return new AppCompatCheckedTextView(this, attrs);
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+        Log.d(TAG, "onPause");
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        if (key.equals(IP_ADDRESS_PORT)) {
+            Preference pref = this.findPreference(key);
+            // Set summary to be the user-description for the selected value
+            pref.setSummary(getResources().getString(R.string.summary_header)
+                    + sharedPreferences.getString(key, Constants.ADDRESS_PORT));
+            Log.d("TAG", pref.getSummary().toString());
+        }
+        Log.d(TAG, "onSharedPreferenceChanged");
+    }
 }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_setting);
+////        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+////        toolbar.setTitle(R.string.app_name);
+////        setSupportActionBar(toolbar);
+//        getFragmentManager().beginTransaction().replace(
+//                android.R.id.content, new MyPreferenceFragment()).commit();
+//    }
+//
+//    public static class MyPreferenceFragment extends PreferenceFragment implements
+//            SharedPreferences.OnSharedPreferenceChangeListener {
+//        @Override
+//        public void onCreate(final Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            addPreferencesFromResource(R.xml.preferences);
+//        }
+////
+//        @Override
+//        public void onResume() {
+//            super.onResume();
+//            getPreferenceScreen().getSharedPreferences()
+//                    .registerOnSharedPreferenceChangeListener(this);
+//            Log.d(TAG, "onResume");
+//        }
+//
+//        @Override
+//        public void onPause() {
+//            super.onPause();
+//            getPreferenceScreen().getSharedPreferences()
+//                    .unregisterOnSharedPreferenceChangeListener(this);
+//            Log.d(TAG, "onPause");
+//        }
+//
+//        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+//                                              String key) {
+//            if (key.equals(IP_ADDRESS_PORT)) {
+//                Preference pref = this.findPreference(key);
+//                // Set summary to be the user-description for the selected value
+//                pref.setSummary(getActivity().getResources().getString(R.string.summary_header)
+//                        + sharedPreferences.getString(key, Constants.ADDRESS_PORT));
+//                Log.d("TAG", pref.getSummary().toString());
+//            }
+//            Log.d(TAG, "onSharedPreferenceChanged");
+//        }
+//    }
+
+
